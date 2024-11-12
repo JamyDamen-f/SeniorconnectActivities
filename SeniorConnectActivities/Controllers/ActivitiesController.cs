db6848dfa879fa0077118fed7e8b9b37f2ef9711
Jamy Damen <jamy_damen@hotmail.com>
2024-11-12 20:06:00 +0100

    Changes- Delete method toegevoegd en aanpassingen gedaan aan add/edit methods

--- SeniorConnectActivities/Controllers/ActivitiesController.cs
+++ SeniorConnectActivities/Controllers/ActivitiesController.cs
@@ -14,6 +14,7 @@ namespace SeniorConnectActivities.Controllers
             _dbConnection = dbConnection;
         }
 
+
         /// <summary>
         /// Get all Activities
         /// </summary>
@@ -65,25 +66,65 @@ namespace SeniorConnectActivities.Controllers
             }
         }
 
-        /// <summary>
-        /// View page with the form to add a activityEntity
-        /// </summary>
-        /// <returns>View with a form</returns>
-        [HttpGet]
-        public IActionResult Add()
-        {
-            return View();
-        }
 
         /// <summary>
         /// Adds a activityEntity
         /// </summary>
-        /// <param name="activity"></param>
+        /// <param name="model"></param>
         /// <returns>A view with the added Activity</returns>
         [HttpPost]
-        public async Task<IActionResult> AddEntity(ActivityModel activity)
+        public async Task<IActionResult> AddEntity(ActivityModel model)
         {
-            if (!ModelState.IsValid)
+            // Open connection
+            var connection = _dbConnection.GetConnection();
+            if (connection.State != ConnectionState.Open)
+            {
+                await connection.OpenAsync();
+            }
+            // Sql command
+            var command = new MySqlCommand("INSERT INTO activity (title, description, location, start, end, max_participants, created_at, updated_at, image_url)\r\n" +
+                                           "VALUES (@title, @description, @location, @start, @end, @max_participants, @created_at, @updated_at, @image_url)", connection);
+
+            // Make a new activitie
+            var AddActivitie = new ActivityModel
+            {
+                Title = model.Title,
+                Description = model.Description,
+                Location = model.Location,
+                Start = model.Start,
+                End = model.End,
+                MaxParticipants = model.MaxParticipants,
+                Created = DateTime.Now,
+                LastUpdated = DateTime.Now,
+            };
+
+
+            // Setting up command parameters
+            command.Parameters.AddWithValue("@title", AddActivitie.Title);
+            command.Parameters.AddWithValue("@description", AddActivitie.Description ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@location", AddActivitie.Location);
+            command.Parameters.AddWithValue("@start", AddActivitie.Start);
+            command.Parameters.AddWithValue("@end", AddActivitie.End);
+            command.Parameters.AddWithValue("@max_participants", AddActivitie.MaxParticipants);
+            command.Parameters.AddWithValue("@created_at", AddActivitie.Created);
+            command.Parameters.AddWithValue("@updated_at", AddActivitie.LastUpdated);
+            command.Parameters.AddWithValue("@image_url", AddActivitie.Url ?? (object)DBNull.Value);
+
+            // Execute the command
+            await command.ExecuteNonQueryAsync();
+
+            // Close connection
+            await connection.CloseAsync();
+            // Return view
+            return RedirectToAction("Index", "Activities");
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> CreateEditEntity(int? id)
+        {
+            if (id != null)
             {
                 // Open connection
                 var connection = _dbConnection.GetConnection();
@@ -92,43 +133,29 @@ namespace SeniorConnectActivities.Controllers
                     await connection.OpenAsync();
                 }
                 // Sql command
-                var command = new MySqlCommand("INSERT INTO activity (title, description, location, start, end, max_participants, created_at, updated_at, image_url)\r\n" +
-                                               "VALUES (@title, @description, @location, @start, @end, @max_participants, @created_at, @updated_at, @image_url)", connection);
-
-                // Make a new activitie
-                var AddActivitie = new ActivityModel
-                {
-                    Title = activity.Title,
-                    Description = activity.Description,
-                    Location = activity.Location,
-                    Start = activity.Start,
-                    End = activity.End,
-                    MaxParticipants = activity.MaxParticipants,
-                    Created = DateTime.Now,
-                    LastUpdated = DateTime.Now,
-                };
-
-
-                // Setting up command parameters
-                command.Parameters.AddWithValue("@title", AddActivitie.Title);
-                command.Parameters.AddWithValue("@description", AddActivitie.Description ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("@location", AddActivitie.Location);
-                command.Parameters.AddWithValue("@start", AddActivitie.Start);
-                command.Parameters.AddWithValue("@end", AddActivitie.End);
-                command.Parameters.AddWithValue("@max_participants", AddActivitie.MaxParticipants);
-                command.Parameters.AddWithValue("@created_at", AddActivitie.Created);
-                command.Parameters.AddWithValue("@updated_at", AddActivitie.LastUpdated);
-                command.Parameters.AddWithValue("@image_url", AddActivitie.Url ?? (object)DBNull.Value);
-
-                // Execute the command
-                await command.ExecuteNonQueryAsync();
-
-                // Close connection
+                var command = new MySqlCommand("SELECT title, description, location, start, end, max_participants, created_at, updated_at, image_url FROM activity WHERE id = @id", connection);
+                // Command parameters
+                command.Parameters.AddWithValue("@id", id);
+                var reader = await command.ExecuteReaderAsync();
+                reader.ReadAsync();
+                // Fill the activity with the id from the list
+                var activity = new ActivityModel();
+                activity.Title = reader.GetString(0);
+                activity.Description = reader.IsDBNull(1) ? null : reader.GetString(1);
+                activity.Location = reader.GetString(2);
+                activity.Start = reader.GetDateTime(3);
+                activity.End = reader.GetDateTime(4);
+                activity.MaxParticipants = reader.GetInt32(5);
+                activity.Created = reader.GetDateTime(6);
+                activity.LastUpdated = reader.GetDateTime(7);
+                activity.Url = reader.IsDBNull(8) ? null : reader.GetString(8);
+                // Close reading and the db connection
+                await reader.CloseAsync();
                 await connection.CloseAsync();
-            }
-            // Return view
-            return RedirectToAction("Index", "Activities");
 
+                return View(activity);
+            }
+            return View();
         }
 
 
@@ -137,7 +164,7 @@ namespace SeniorConnectActivities.Controllers
         /// </summary>
         /// <returns>returns the details page with the selected entity</returns>
         [HttpGet]
-        public async Task<IActionResult> Details(int id)
+        public async Task<IActionResult> DetailsEntity(int id)
         {
             // Opens db connection
             var connection = _dbConnection.GetConnection();
@@ -172,6 +199,14 @@ namespace SeniorConnectActivities.Controllers
             // Return view 
             return View("Details", activity);
         }
+
+
+        
+        public IActionResult DeleteEntity(int id)
+        {
+            return RedirectToAction("Index", "Activities");
+        }
+
     }
 }
 
--- SeniorConnectActivities/Models/Entities/ActivityModel.cs
+++ SeniorConnectActivities/Models/Entities/ActivityModel.cs
@@ -5,11 +5,18 @@ namespace SeniorConnectActivities.Models.Entities
     public class ActivityModel
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Location { get; set; }
+        [Required]
         public DateTime Start { get; set; }
+        [Required]
         public DateTime End { get; set; }
+        [Required]
+        [StringLength(200)]
         public int MaxParticipants { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastUpdated { get; set; }
